19eebed5a803cb536b2dfabb50e572b1fb4b2935
Entegrio <ekpyrosis180@gmail.com>
2022-06-13 20:01:53 +0300

    IntVars.ccp work with integer variables

---
+++ C++/LaforeObjectOrientedProgramming/Chapter1/FIRST/IntVars/IntVars/IntVars.cpp
@@ -0,0 +1,20 @@
+// intvars.ccp
+// work with integer variables
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var1;
+    int var2;
+
+    var1 = 20;
+    var2 = var1 + 10;
+
+    cout << "va1 + 10 = ";
+    cout << var2 << endl;
+
+    return 0;
+}
+
+
