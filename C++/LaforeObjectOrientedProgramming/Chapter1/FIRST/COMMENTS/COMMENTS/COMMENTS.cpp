e8240fedc14da87221dc5b872c08a93c3bbdf98b
Entegrio <ekpyrosis180@gmail.com>
2022-06-13 17:52:59 +0300

    coments. app Program demonstrates the use of comments.

---
+++ C++/LaforeObjectOrientedProgramming/Chapter1/FIRST/COMMENTS/COMMENTS/COMMENTS.cpp
@@ -0,0 +1,11 @@
+// coments.cpp
+// demonstrates the use of comments 
+#include <iostream>     // the preprocessor directive
+using namespace std;    // using directive
+                
+int main()              // function named main
+{                       // begin of function body
+    cout << "Every era has its own language\n"; // operator
+    return 0;           // operator
+}                       // end of functon body
+
