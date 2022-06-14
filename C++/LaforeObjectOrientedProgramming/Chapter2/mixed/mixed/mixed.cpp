11aed14406a80aa12d53a3820697b63e6157d96d
Entegrio <ekpyrosis180@gmail.com>
2022-06-14 10:48:16 +0300

    mixed.cpp Using a mixed expression

---
+++ C++/LaforeObjectOrientedProgramming/Chapter2/mixed/mixed/mixed.cpp
@@ -0,0 +1,18 @@
+// mixed.cpp
+// Using a mixed expression
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 7;
+    float avgWeight = 155.5F;
+
+    double totalWeght = count * avgWeight;
+
+    cout << "The weight is " << totalWeght << endl;
+
+    return 0;
+}
+
