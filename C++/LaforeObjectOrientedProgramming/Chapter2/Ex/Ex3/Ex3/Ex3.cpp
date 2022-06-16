7354b4ad94632fc4ff459088776da0c43148b12b
Entegrio <ekpyrosis180@gmail.com>
2022-06-16 17:21:41 +0300

    Ex3.cpp

---
+++ C++/LaforeObjectOrientedProgramming/Chapter2/Ex/Ex3/Ex3/Ex3.cpp
@@ -0,0 +1,17 @@
+// Ex3.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int intConst = 10;
+    int arithOp = intConst + 10;
+
+    cout << intConst << endl;
+    cout << arithOp << endl;
+    cout << --arithOp << endl;
+
+    return 0;
+}
+
