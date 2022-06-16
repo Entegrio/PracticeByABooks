dfae13d05595440985cf769af71aae337a9ea97f
Entegrio <ekpyrosis180@gmail.com>
2022-06-16 17:30:41 +0300

    Ex4.cpp

---
+++ C++/LaforeObjectOrientedProgramming/Chapter2/Ex/Ex4/Ex4/Ex4.cpp
@@ -0,0 +1,22 @@
+// Ex4.cpp
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "URNS and odours bring away!" << "\n" << endl
+		<< "Vapours, sighs, darken the day! " << "\n" << endl
+		<< "Our dole more deadly looks than dying; " << "\n" << endl
+		<< "Balms and gums and heavy cheers, " << "\n" << endl
+		<< "Sacred vials fill'd with tears, " << "\n" << endl
+		<< "And clamours through the wild air flying!" << "\n" << endl << endl
+
+		<< "Come, all sad and solemn shows," << "\n" << endl
+		<< "That are quick-eyed Pleasure's foes! " << "\n" << endl
+		<< "We convent naught else but woes. " << "\n" << endl;
+
+	return 0;
+}
+
