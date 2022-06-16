7185d224e66500c037d7380fcb680dc78b5262f7
Entegrio <ekpyrosis180@gmail.com>
2022-06-16 23:28:05 +0300

    Ex11.cpp upd

--- C++/LaforeObjectOrientedProgramming/Chapter2/Ex/Ex11/Ex11/Ex11.cpp
+++ C++/LaforeObjectOrientedProgramming/Chapter2/Ex/Ex11/Ex11/Ex11.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    cout  << setw(12) << "Surname" << setw(12) << "Name" << setw(16) << "Address" << setw(12) << "City" << endl
+    cout << setiosflags(ios::left) << setw(12) << "Surname" << setw(12) << "Name" << setw(16) << "Address" << setw(12) << "City" << endl
         << setfill('-') << setw(52) << "-" << endl
         << setfill(' ') << setw(12) << "White" << setw(12) << "Mike" << setw(16) << "Grean St. 34" << setw(12) << "New York" << endl
         << setw(12) << "Dice" << setw(12) << "Rob" << setw(16) << "Saint St. 61" << setw(12) << "Chicago" << endl;
