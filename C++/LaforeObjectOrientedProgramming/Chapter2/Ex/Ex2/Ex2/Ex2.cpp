5fb4bb7f3c4542ca8a3f6b717e98c4e34a05cb12
Entegrio <ekpyrosis180@gmail.com>
2022-06-16 14:34:05 +0300

    Ex2.cpp

---
+++ C++/LaforeObjectOrientedProgramming/Chapter2/Ex/Ex2/Ex2/Ex2.cpp
@@ -0,0 +1,13 @@
+// Ex2.cpp
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << 1990 << setw(12) << 135 << endl
+        << 1991 << setw(12) << 7290 << endl
+        << 1992 << setw(12) << 11300 << endl
+        << 1993 << setw(12) << 16200 << endl;
+}
