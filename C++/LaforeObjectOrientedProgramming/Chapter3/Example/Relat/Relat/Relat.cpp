9ac5f5f4851a88363a83e25ceeb182bcb78cf6f1
Entegrio <ekpyrosis180@gmail.com>
2022-06-18 22:57:44 +0300

    Reorganizing the file structure and Relat.cpp Using a relation operation

---
+++ C++/LaforeObjectOrientedProgramming/Chapter3/Example/Relat/Relat/Relat.cpp
@@ -0,0 +1,20 @@
+// Relat.cpp 
+// Using a relation operation 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numb;
+
+    cout << "Enter a number: ";
+    cin >> numb;
+
+    cout << "numb < 10 is " << (numb < 10) << endl;
+    cout << "numb > 10 is " << (numb > 10) << endl;
+    cout << "numb == 10 is " << (numb == 10) << endl;
+
+    return 0;
+}
+
