a546fbde6518ad177f4b98bba4d1aa3c3c60f90e
Entegrio <ekpyrosis180@gmail.com>
2022-06-18 23:13:19 +0300

    ForDemo.cpp Demonstrating of the simplest for loop.

---
+++ C++/LaforeObjectOrientedProgramming/Chapter3/Example/ForDemo/ForDemo/ForDemo.cpp
@@ -0,0 +1,17 @@
+// ForDemo.cpp
+// Demonstrating of the simplest for loop.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int j;
+    for (j = 0; j < 15; j++)
+        cout << j * j << " ";
+    cout << endl;
+
+    return 0;
+}
+
+
