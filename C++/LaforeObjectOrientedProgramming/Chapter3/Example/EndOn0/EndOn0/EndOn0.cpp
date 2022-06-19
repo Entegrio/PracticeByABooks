b53df09885d230484c98d54fbd5d89b93b7368a3
Entegrio <ekpyrosis180@gmail.com>
2022-06-19 20:30:10 +0300

    EndOn0.cpp Using the cycle while

---
+++ C++/LaforeObjectOrientedProgramming/Chapter3/Example/EndOn0/EndOn0/EndOn0.cpp
@@ -0,0 +1,17 @@
+// EndOn0.cpp
+// Using the cycle while.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 99;		// the n must not be 0 when starting the cycle.
+
+	while (n != 0)
+		cin >> n;
+
+	cout << endl;
+
+	return 0;
+}
