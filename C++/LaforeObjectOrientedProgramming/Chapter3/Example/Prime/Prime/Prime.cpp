72a282d2cc53b455c8aa965d2357dcabdf3b8fa0
Entegrio <ekpyrosis180@gmail.com>
2022-06-22 08:45:33 +0300

    Prime.cpp upd My own vision.

--- C++/LaforeObjectOrientedProgramming/Chapter3/Example/Prime/Prime/Prime.cpp
+++ C++/LaforeObjectOrientedProgramming/Chapter3/Example/Prime/Prime/Prime.cpp
@@ -7,19 +7,22 @@ using namespace std;
 int main()
 {
     unsigned long n, j;
+    unsigned int counter = 0;
 
     cout << "Enter a number: ";
     cin >> n;
 
     for (j = 2; j <= n/2; j++)
-    {
         if (n % j == 0)
         {
-            cout << "It's not prime; divisible by " << j << endl;
-            exit(0);
+            cout << "Divisible by " << j << endl;
+            counter++;
         }
-    }
-    cout << "It's prime\n";
+
+    if (counter == 0)
+        cout << "It's prime\n";
+    else
+        cout << "It's not prime\n";
 
     return 0;
 }
