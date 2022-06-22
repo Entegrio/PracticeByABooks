1bfa3b859e355a432c4defdc8dac87298d3fd626
Entegrio <ekpyrosis180@gmail.com>
2022-06-22 08:37:45 +0300

    Prime.cpp demonstrates IF statement with prime numbers;

---
+++ C++/LaforeObjectOrientedProgramming/Chapter3/Example/Prime/Prime/Prime.cpp
@@ -0,0 +1,25 @@
+// Prime.cpp 
+// demonstrates IF statement with prime numbers;
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned long n, j;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for (j = 2; j <= n/2; j++)
+    {
+        if (n % j == 0)
+        {
+            cout << "It's not prime; divisible by " << j << endl;
+            exit(0);
+        }
+    }
+    cout << "It's prime\n";
+
+    return 0;
+}
